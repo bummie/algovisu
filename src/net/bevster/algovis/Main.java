ddb9a897583ac08994f23f370f6db1ed3c67464e
Seb <seebzei@gmail.com>
2017-08-21 22:15:52 +0200

    Fungerende bubblesort

--- src/net/bevster/algovis/Main.java
+++ src/net/bevster/algovis/Main.java
@@ -2,18 +2,27 @@ package net.bevster.algovis;
 
 import java.awt.*;
 import java.awt.image.BufferedImage;
+import java.util.Random;
 import javax.swing.JFrame;
 
 
 public class Main extends JFrame
 {
 	private boolean isRunning = true;
-	private final int FRAMES_PER_SECOND = 30;
+	private final int FRAMES_PER_SECOND = 100;
 	private int windowWidth = 720;
 	private int windowHeight = 480;
 	private long time;
 
-	private int[] arrayVerdier = {10, 1, 5, 4, 9, 3,7,2,6};
+	private Font textFont = new Font ("Courier New", Font.BOLD, 17);
+
+
+	private final int ARRAY_SIZE = 150;
+	private int arrayPos = 0, lastSortedOrigin = 0;
+	private boolean swappedInt = true, isSorted = false;
+	private int[] arrayVerdier;
+
+	private int statSwaps = 0;
 
 	BufferedImage backBuffer;
 	Insets insets;
@@ -27,6 +36,8 @@ public class Main extends JFrame
 
 	private void init()
 	{
+		hentTilfeldigeArrayVerdier(ARRAY_SIZE);
+
 		setTitle("Algoritmevisualisering");
 		setSize(windowWidth, windowHeight);
 		setResizable(false);
@@ -66,7 +77,27 @@ public class Main extends JFrame
 
 	private void update()
 	{
-		// Oppdater grafer her
+		if(!isSorted)
+		{
+
+			if(arrayPos >= arrayVerdier.length-1)
+			{
+				arrayPos = 0;
+				if(swappedInt = false)
+					isSorted = true;
+				swappedInt = false;
+			}
+
+			if(arrayVerdier[arrayPos] > arrayVerdier[arrayPos+1])
+			{
+				int buffer = arrayVerdier[arrayPos];
+				arrayVerdier[arrayPos] = arrayVerdier[arrayPos+1];
+				arrayVerdier[arrayPos+1] = buffer;
+				swappedInt = true;
+				statSwaps++;
+			}
+			arrayPos++;
+		}
 	}
 
 	private void draw()
@@ -81,20 +112,41 @@ public class Main extends JFrame
 		// Tegn søyler
 		drawGraphs(bbg);
 
+		// Statestikktekst
+		g.setColor(Color.black);
+		g.setFont(textFont);
+		g.drawString("Swaps: " + statSwaps , 100, 100);
+
 		g.drawImage(backBuffer, insets.left, insets.top, this);
 	}
 
 	private void drawGraphs(Graphics g)
 	{
-		g.setColor(Color.BLACK);
 
 		int barWidth = windowWidth / arrayVerdier.length;
+		int barHeightMultiplier = (windowHeight/arrayVerdier.length);
 		for(int i = 0; i < arrayVerdier.length; i++)
 		{
-			int barHeight = arrayVerdier[i]*10;
+			if(i%2==0)
+				g.setColor(Color.BLACK);
+			else
+				g.setColor(Color.CYAN);
+
+			int barHeight = arrayVerdier[i]*barHeightMultiplier;
 			int y = windowHeight - barHeight;
 			g.fillRect(i*barWidth, y, barWidth, barHeight);
 		}
 
+		g.setColor(Color.RED);
+		g.fillRect(arrayPos*barWidth, 0, barWidth, windowHeight);
+	}
+
+	private void hentTilfeldigeArrayVerdier(int size)
+	{
+		arrayVerdier = new int[size];
+		for(int i = 0; i < size; i++)
+		{
+			arrayVerdier[i] = new Random().nextInt(size);
+		}
 	}
 }
